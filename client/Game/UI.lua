b87ab4716eff21dc812cfa7ea6b2542a8b8cdb0a
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2022-04-25 18:05:41 +0300

    add untracked crucial file

---
+++ client/Game/UI.lua
@@ -0,0 +1,38 @@
+local Debris = game:GetService("Debris")
+
+local UI = {}
+UI.StatusBarMessages = {
+    InProgress = {Text = "A round is already in progress - wait for the next one", Color = Color3.fromRGB(255, 219, 34)}
+}
+
+--TODO: port over UI code from minesweeper client
+function UI.statusBar(text, color)
+    
+end
+
+function UI.createMessage(text, color, duration)
+    assert(text ~= nil, "text cannot be empty")
+    assert(type(text) == "string", "text must be a string")
+
+    local messages = UI.ScreenInstance.MessageList
+    local messageTemplate = messages.MessageTemplate:Clone()
+    color = color or Color3.fromRGB(255,255,255)
+    duration = duration or 30
+
+    local message = messageTemplate:Clone()
+    message.Text = text
+    message.TextColor3 = color
+    message.Visible = true
+    message.Name = text
+    message.Parent = messages
+    Debris:AddItem(message, duration)
+    
+end
+
+return function(game)
+    UI.Game = game
+    UI.Instance = game.Gui
+    UI.ScreenInstance = UI.Instance:WaitForChild("Screen")
+    
+    return UI
+end
