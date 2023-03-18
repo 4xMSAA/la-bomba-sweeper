343fa2369f7089074cdae9152d2463ca98f4148e
Mia Soomre <27443298+4xMSAA@users.noreply.github.com>
2023-03-18 15:08:09 +0200

    remove irrelevant enum for minesweeper

--- enums/CameraOffset.lua
+++ enums/CameraOffset.lua
@@ -3,7 +3,6 @@ local TableUtils = require(shared.Common.TableUtils)
 ---@class CameraOffset
 local GameEnum = {
     {"Animation", "Affected by animating the camera"},
-    {"Recoil", "Affected by weapon recoil"}
 }
 
 return TableUtils.toEnumList(script.Parent.Name, GameEnum)
